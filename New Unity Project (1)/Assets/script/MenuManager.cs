e27e218321a3c611ef34fc4aef54d4c4bb07455a
betty0221a <betty0221a@gmail.com>
2020-11-14 20:14:53 +0800

    完成初步寫程式

---
+++ New Unity Project (1)/Assets/script/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+
+           
+    }
+
+    // Update is called once per frame
+   public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+}
