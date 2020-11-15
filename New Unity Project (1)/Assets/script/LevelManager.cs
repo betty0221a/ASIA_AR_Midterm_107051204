e4fc5a7a23c4d2e81cf739655329bc0e4ff7dc39
betty0221a <betty0221a@gmail.com>
2020-11-15 21:43:49 +0800

    so far完成

---
+++ New Unity Project (1)/Assets/script/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+public void NextLevel()
+{
+
+}
+public void BackToMenu()
+{
+        SceneManager.LoadScene("選單");
+}
+public void QuitGame()
+{
+        Application.Quit();
+}
+
+}
---
+++ New Unity Project (1)/Assets/script/Player.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public Text textCount;
+
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "傳送門") ;
+        print("碰到傳送門了");
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "金幣")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "金幣數量"+count;
+        }
+    }
+
+
+
+
+}
